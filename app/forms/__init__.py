6bc3872b0df119b599f5fe4a687839f0076a5541
nasif raihan <nasif.raihan78@gmail.com>
2024-03-25 22:24:57 +0600

    implemented document_create_view

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,2 +1,4 @@
-from .registration_form import RegistrationForm
+from .document_form import DocumentForm
 from .login_form import LoginForm
+from .registration_form import RegistrationForm
+from .share_document_form import ShareDocumentForm
