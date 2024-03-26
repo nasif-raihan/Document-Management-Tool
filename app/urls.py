7ba7c4fbc53eb3c1d16d49e01707aa90b155acf4
nasif raihan <nasif.raihan78@gmail.com>
2024-03-26 22:06:50 +0600

    fixed urls file

--- app/urls.py
+++ app/urls.py
@@ -19,19 +19,10 @@ urlpatterns = [
         name="logout",
     ),
     # fmt: off
-
     # Document URLS
-    path('documents/', app_views.DocumentView.as_view(), name='document-list'),
-    path('documents/create/', app_views.DocumentView.as_view(), name='document-create'),
-    path('documents/<int:pk>/', app_views.DocumentView.as_view(), name='document-detail'),
-    path('documents/<int:pk>/update/', app_views.DocumentView.as_view(), name='document-update'),
-    path('documents/<int:pk>/delete/', app_views.DocumentView.as_view(), name='document-delete'),
+    path('documents/', app_views.DocumentView.as_view(), name='document'),
 
     # Share Details URLS
-    path('share-details/', app_views.ShareDocumentView.as_view(), name='share-details-list'),
-    path('share-details/create/', app_views.ShareDocumentView.as_view(), name='share-details-create'),
-    path('share-details/<int:pk>/', app_views.ShareDocumentView.as_view(), name='share-details-detail'),
-    path('share-details/<int:pk>/update/', app_views.ShareDocumentView.as_view(), name='share-details-update'),
-    path('share-details/<int:pk>/delete/', app_views.ShareDocumentView.as_view(), name='share-details-delete'),
-    # fmt: on
+    path('share-details/', app_views.ShareDocumentView.as_view(), name='share-details'),
+    # fmt: off
 ]
