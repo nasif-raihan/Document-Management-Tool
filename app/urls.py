abf6096c96f8cfaccd9a4ac918cc3b3a2f040e55
nasif raihan <nasif.raihan78@gmail.com>
2024-03-26 13:40:40 +0600

    fixed urls file

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from app import forms, views
+from app import forms
+from app import views as app_views
 
 urlpatterns = [
     path(
@@ -16,26 +17,20 @@ urlpatterns = [
         auth_views.LogoutView.as_view(next_page="login"),
         name="logout",
     ),
-    path("", views.DocumentListView.as_view(), name="document_list"),
-    path(
-        "document/<int:pk>/", views.DocumentDetailView.as_view(), name="document_detail"
-    ),
-    path(
-        "document/create/", views.DocumentCreateView.as_view(), name="document_create"
-    ),
-    path(
-        "document/<int:pk>/update/",
-        views.DocumentUpdateView.as_view(),
-        name="document_update",
-    ),
-    path(
-        "document/<int:pk>/delete/",
-        views.DocumentDeleteView.as_view(),
-        name="document_delete",
-    ),
-    path(
-        "document/<int:pk>/share/",
-        views.ShareDocumentView.as_view(),
-        name="share_document",
-    ),
+    # fmt: off
+
+    # Document URLS
+    path('documents/', app_views.DocumentView.as_view(), name='document-list'),
+    path('documents/create/', app_views.DocumentView.as_view(), name='document-create'),
+    path('documents/<int:pk>/', app_views.DocumentView.as_view(), name='document-detail'),
+    path('documents/<int:pk>/update/', app_views.DocumentView.as_view(), name='document-update'),
+    path('documents/<int:pk>/delete/', app_views.DocumentView.as_view(), name='document-delete'),
+
+    # Share Details URLS
+    path('share-details/', app_views.ShareDocumentView.as_view(), name='share-details-list'),
+    path('share-details/create/', app_views.ShareDocumentView.as_view(), name='share-details-create'),
+    path('share-details/<int:pk>/', app_views.ShareDocumentView.as_view(), name='share-details-detail'),
+    path('share-details/<int:pk>/update/', app_views.ShareDocumentView.as_view(), name='share-details-update'),
+    path('share-details/<int:pk>/delete/', app_views.ShareDocumentView.as_view(), name='share-details-delete'),
+    # fmt: on
 ]
