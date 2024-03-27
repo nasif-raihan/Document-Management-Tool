c2a3a82226a16876013635a1c1c91fe0277ced8c
nasif raihan <nasif.raihan78@gmail.com>
2024-03-27 22:41:14 +0600

    registered models to admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Document, UserAccessDetail
 
-# Register your models here.
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "content",
+        "created_at",
+        "modified_at",
+        "owner",
+    )
+
+
+@admin.register(UserAccessDetail)
+class UserAccessDetailAdmin(admin.ModelAdmin):
+    list_display = ("document", "user", "permission_type")
--- app/models.py
+++ app/models.py
@@ -7,8 +7,12 @@ class Document(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    owner = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='owned_document')
-    shared_with = models.ManyToManyField(to=User, through="UserAccessDetail", related_name='shared_documents')
+    owner = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name="owned_document"
+    )
+    shared_with = models.ManyToManyField(
+        to=User, through="UserAccessDetail", related_name="shared_documents"
+    )
 
     def __str__(self):
         return f"{self.title}"
@@ -27,3 +31,6 @@ class UserAccessDetail(models.Model):
     class Meta:
         # Ensure each user has only one permission per document
         unique_together = ("document", "user")
+
+    def __str__(self):
+        return f"{self.permission_type}"
